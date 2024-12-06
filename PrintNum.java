68aa4beca9e608d0e0b229e85580aa8e9f493254
RAMIREDDYDEEPIKA <ramireddydeepika@gmail.com>
2024-12-06 10:48:16 +0530

    printing numbers without using loops

---
+++ PrintNum.java
@@ -0,0 +1,16 @@
+package listInterfaceOperations;
+
+public class PrintNum
+{
+    public static void main(String[] args)
+    {
+        printNum(1);
+    }
+    public static void printNum(int num)
+    {
+        if(num<10) {
+            System.out.println(num + " ");
+            printNum(num + 1);
+        }
+    }
+}
